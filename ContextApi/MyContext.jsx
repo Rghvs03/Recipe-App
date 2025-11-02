0d1d8009353b5d95f2268cbe6ffc7010190f60fe
Rghvs03 <raghav437@gmail.com>
2025-11-02 16:00:53 +0530

    tried again

--- ContextApi/MyContext.jsx
+++ ContextApi/MyContext.jsx
@@ -1,20 +1,29 @@
+import { LoaderPinwheelIcon } from "lucide-react";
 import { createContext, useState, useEffect } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const MyStore = createContext();
 
 export const MyContextProvider = ({ children }) => {
-  const [recipes, setRecipes] = useState(
-    () => JSON.parse(localStorage.getItem("recipes")) || []
-  );
-
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const storedRecipes = localStorage.getItem("recipes");
     if (storedRecipes) {
       setRecipes(JSON.parse(storedRecipes));
     }
+    setLoading(false);
   }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center">
+        {" "}
+        <LoaderPinwheelIcon size={28} className="animate-spin" />{" "}
+      </div>
+    ); // Or render a loading spinner
+  }
 
   return (
     <MyStore.Provider value={{ recipes, setRecipes }}>
