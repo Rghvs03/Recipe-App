1c102a225a691b1f98656e9a281366ae448c9372
Rghvs03 <raghav437@gmail.com>
2025-11-02 11:19:44 +0530

    tried issue fixed

--- ContextApi/MyContext.jsx
+++ ContextApi/MyContext.jsx
@@ -1,11 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-export let MyStore = createContext();
+// eslint-disable-next-line react-refresh/only-export-components
+export const MyStore = createContext();
 
 export const MyContextProvider = ({ children }) => {
   const [recipes, setRecipes] = useState(
     () => JSON.parse(localStorage.getItem("recipes")) || []
   );
+
+
+  useEffect(() => {
+    const storedRecipes = localStorage.getItem("recipes");
+    if (storedRecipes) {
+      setRecipes(JSON.parse(storedRecipes));
+    }
+  }, []);
 
   return (
     <MyStore.Provider value={{ recipes, setRecipes }}>
